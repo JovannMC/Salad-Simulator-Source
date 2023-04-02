273d6033c1fae1a3c1f02f2c4f704758c8cb8d12
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-04-02 17:15:36 +0300

    Fix bugs
    + Fix browser being empty when closing and reopening
    + Fix VersionText being unreliable

--- Assets/Scripts/VersionText.cs
+++ Assets/Scripts/VersionText.cs
@@ -9,15 +9,15 @@ public class VersionText : MonoBehaviour
         
         TMP_Text versionText = GetComponent<TextMeshProUGUI>();
         
-        if (Application.version.Contains("a"))
+        if (Application.version.Contains("pb"))
+        {
+            versionText.text = "Version: " + Application.version + " (Private Build)";
+        } else if (Application.version.Contains("a"))
         {
             versionText.text = "Version: " + Application.version + " (Alpha)";
         } else if (Application.version.Contains("b"))
         {
             versionText.text = "Version: " + Application.version + " (Beta)";
-        } else if (Application.version.Contains("pb"))
-        {
-            versionText.text = "Version: " + Application.version + " (Private Build)";
         } else
         {
             versionText.text = "Version: " + Application.version;
